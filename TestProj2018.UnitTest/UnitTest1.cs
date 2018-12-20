1d3bdfe999574879c474ba47eaf637c3b86e1d86
RomanKM14R <RomanKM14R@ya.ru>
2018-12-20 12:21:35 +0300

    Circle, Triangle and unitTests are done

---
+++ TestProj2018.UnitTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProj2018.UnitTest
+{
+    [TestClass]
+    public class FiguresTests
+    {
+        [TestMethod]
+        public void TriangleTest()
+        {
+            double sideA = 10;
+            double sideB = 10;
+            double sideC = 10;
+            double expected = 489.89794855663564;
+            TestProj2018.Figure figure1 = new TestProj2018.Triangle(sideA, sideB, sideC);
+            double actual = figure1.CalcArea();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CircleTest() {
+            double radius = 10;
+            double expected = 314.15926535897933;
+            double actual = new TestProj2018.Circle(radius).CalcArea();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ TestProj2018/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProj2018
+{
+    /// <summary>
+    /// Main operation class (accordingly represent a basic figure)
+    /// </summary>
+    public abstract class Figure
+    {
+        abstract public double CalcArea();
+    }
+
+    /// <summary>
+    /// Triangle class
+    /// </summary>
+    public class Triangle : Figure
+    {
+        double sideA, sideB, sideC;
+        /// <summary>
+        /// Default constructor crteates one cm side triangle
+        /// </summary>
+        public Triangle() { sideA = 1; sideB = 1; sideC = 1; }
+        /// <summary>
+        /// Creates triangle
+        /// </summary>
+        /// <param name="sideA">Dimension in centimeters</param>
+        /// <param name="sideB">Dimension in centimeters</param>
+        /// <param name="sideC">Dimension in centimeters</param>
+        public Triangle(double sideA, double sideB, double sideC) { this.sideA = sideA; this.sideB = sideB; this.sideC = sideC; }
+        public override double CalcArea()
+        {
+            double semiPerimeter = sideA + sideB + sideC;
+            return Math.Pow(semiPerimeter * (semiPerimeter - sideA) * (semiPerimeter - sideB) * (semiPerimeter - sideC), 0.5);
+        }
+    }
+
+    /// <summary>
+    /// Circle class
+    /// </summary>
+    public class Circle : Figure
+    {
+        double radius;
+        public Circle(double radius) { this.radius = radius; }
+        public override double CalcArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+}
